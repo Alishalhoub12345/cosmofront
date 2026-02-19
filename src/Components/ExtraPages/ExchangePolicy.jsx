29c500140ed4655741d29e89aab5735d21acda34
Alishalhoub12345 <alishalhoub444@gmail.com>
2026-02-19 12:35:59 +0200

    Initial commit

--- src/Components/ExtraPages/ExchangePolicy.jsx
+++ src/Components/ExtraPages/ExchangePolicy.jsx
@@ -16,9 +16,8 @@ function ExchangePolicy() {
   return (
     <div
       dir={isArabic ? "rtl" : "ltr"}
-      className={`text-[#082252] w-full min-h-[80vh] px-[5%] pt-[2%] font-[FrutigerLTCom-Roman] lg:py-[2%] ${
-        isArabic ? "text-right text-[20px] lg:text-[15px]" : ""
-      }`}
+      className={`text-[#082252] w-full min-h-[80vh] px-[5%] pt-[2%] font-[FrutigerLTCom-Roman] lg:py-[2%] ${isArabic ? "text-right text-[20px] lg:text-[15px]" : ""
+        }`}
     >
       <div className="px-[2%]">
         {/* TITLE */}
@@ -26,26 +25,11 @@ function ExchangePolicy() {
           {t("exchangePolicy.titleMain")}
         </h1>
 
-        {/* INTRO */}
+        {/* INTRO 
         <div className={`text-[15px] py-[1%] ${isArabic ? "" : "w-[80%]"}`}>
-          <p className={`${isArabic ? "" : "w-[46vw]"}`}>
-            {t("exchangePolicy.p1")}
-          </p>
 
-          <p>{t("exchangePolicy.p2")}</p>
-
-          <p
-            dangerouslySetInnerHTML={{ __html: t("exchangePolicy.p3") }}
-          />
-
-          <p
-            className={`${isArabic ? "" : "w-[47vw]"}`}
-            dangerouslySetInnerHTML={{ __html: t("exchangePolicy.p4") }}
-          />
-
-          <p>{t("exchangePolicy.p5")}</p>
         </div>
-
+        */}
         {/* SECTIONS */}
         <div className="flex flex-col text-[15px]">
           {/* SECTION 1 */}
@@ -54,11 +38,27 @@ function ExchangePolicy() {
               <strong>{t("exchangePolicy.section1.section1Title")}</strong>
             </p>
             <p>{t("exchangePolicy.section1.sec1p1")}</p>
+            <p className={`${isArabic ? "" : "w-[46vw]"}`}>
+              {t("exchangePolicy.p1")}  
+            </p>
+            <br/>
+            <p>{t("exchangePolicy.p2")}</p>
+
+            <p
+              dangerouslySetInnerHTML={{ __html: t("exchangePolicy.p3") }}
+            />
+
+            <p
+              className={`${isArabic ? "" : "w-[47vw]"}`}
+              dangerouslySetInnerHTML={{ __html: t("exchangePolicy.p4") }}
+            />
+
+            <p>{t("exchangePolicy.p5")}</p>
           </div>
 
           {/* SECTION 2 */}
-          <div className="pt-[2%]">
-            <p className="md:py-[5%]">
+          <div className="mt-[1%]">
+            <p className="">
               <strong>{t("exchangePolicy.section2.section2Title")}</strong>
             </p>
 
@@ -84,3 +84,4 @@ function ExchangePolicy() {
 }
 
 export default ExchangePolicy;
+
